38ebe7018732924a4dfe26c96f9f10eaa30b7d65
Yash Shukla <126411225+ThePragmaticProgrammer007@users.noreply.github.com>
2025-04-10 12:26:54 +0000

    lecture 6 and 7 done(dealing with dataTypes and conversion Operations)

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,111 @@
+/*let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let score1 = "33"
+
+console.log(typeof score1);
+console.log(typeof(score1));
+
+let score2 = "33"
+let valueInNumber = Number(score2)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let score3 = "33abc"
+let valueInNumber1 = Number(score3)
+
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1); 
+
+let score4 = null
+let valueInNumber2 = Number(score4);
+
+console.log(typeof score4);
+console.log(score4);
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
+//console.log("\n");
+
+let score5 = undefined
+let valueInNumber3 = Number(score5);
+
+console.log(typeof score5);
+console.log(score5);
+console.log(typeof valueInNumber3);
+console.log(valueInNumber3);
+
+//console.log("\n");
+
+let score6 = "Yash"
+let valueInNumber4 = Number(score6);
+
+console.log(typeof score6);
+console.log(score6);
+console.log(typeof valueInNumber4);
+console.log(valueInNumber4); 
+
+let isLoggedIn = "Yash"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+let isLoggedIn1 = 1
+
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber); */ 
+
+// *********************** Operations **********
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Yash "
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+console.log(+false);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
